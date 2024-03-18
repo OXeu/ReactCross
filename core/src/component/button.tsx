60f916e4f4a4081ea2dcfc65d6307f5b70810c22
Xeu <thankrain@qq.com>
2024-03-18 11:42:14 +0800

    fix: yarn workspace error

--- core/src/component/button.tsx
+++ core/src/component/button.tsx
@@ -3,7 +3,7 @@ import { getComponentFactory } from "..";
 export function QButton(props: { children: string }) {
     const { QText, QColumn } = getComponentFactory();
     return (
-        <QColumn className="px-8 py-4 bg-blue-500 rounded-xl hover:bg-blue-600">
+        <QColumn className="px-8 py-4 bg-blue-500 rounded-xl active:bg-blue-600 active:scale-95">
             <QText className="text-white">
                 {props.children}
             </QText>
