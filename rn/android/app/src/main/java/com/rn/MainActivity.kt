8852fc0a6f44b4d7507f30f5dcb0db2e08310116
Xeu <thankrain@qq.com>
2024-03-20 13:15:17 +0800

    feat: support React Navigation feat: new navigation abstraction

--- rn/android/app/src/main/java/com/rn/MainActivity.kt
+++ rn/android/app/src/main/java/com/rn/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.rn
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +19,8 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
--- rn/index.js
+++ rn/index.js
@@ -2,10 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
 import ReactNativeFeatureFlags from 'react-native/Libraries/ReactNative/ReactNativeFeatureFlags';
+import { name as appName } from './app.json';
+import App from './src/App';
 
 // enable the JS-side of the w3c PointerEvent implementation
 ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = () => true;
@@ -13,6 +13,6 @@ ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = () => true;
 // enable hover events in Pressibility to be backed by the PointerEvent implementation.
 // shouldEmitW3CPointerEvents should also be true
 ReactNativeFeatureFlags.shouldPressibilityUseW3CPointerEventsForHover = () =>
-  true;
+    true;
 
 AppRegistry.registerComponent(appName, () => App);
